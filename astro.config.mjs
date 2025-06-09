97e54b42fe7e0e3addf6e01977d805794cbd601d
Yngvi <yngvi.works@gmail.com>
2025-06-09 17:55:07 -0300

    Ajsutes seo e aria label

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-    plugins: [tailwindcss()],
+  site: 'https://lpsell.com.br', // Substitua pelo seu domínio real se for diferente
+  integrations: [
+    sitemap()
+  ],
+  vite: {
+    plugins: [
+      tailwindcss({
+        configFile: './tailwind.config.cjs' // Adicionado para especificar o arquivo de configuração
+      })
+    ],
   },
 });
